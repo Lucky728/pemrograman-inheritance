ad2a54edb04e66ee9dac57078e941f5ac476850c
Lucky728 <61776822+Lucky728@users.noreply.github.com>
2020-04-11 11:36:51 +0700

    Update pemrograman.cs

--- pemrograman.cs
+++ pemrograman.cs
@@ -10,13 +10,13 @@ namespace LatihanInheritance
     {
         static void Main(string[] args)
         {
-            Person person = new Person("Herjuno", 19);
+            Person person = new Person("Lucky", 19);
             person.GetNameAndAge();
             Console.WriteLine();
             Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
             teacher.GetNameAndAge();
             Console.WriteLine();
-            Student student = new Student("Herjuno", 19, "19.11.2644", "herjuno.nugroho@students.amikom.ac.id");
+            Student student = new Student("Lucky", 19, "19.11.2609", "lucky.yudhistira@students.amikom.ac.id");
             student.GetNameAndAge();
 
             Console.ReadKey();
